a7fa5c47b226881fb2480460592b91457c751c95
Trinh Vu <txvu100@gmail.com>
2022-12-05 21:36:47 -0600

    Added DataTileComp

--- src/components/DataTitleComp.vue
+++ src/components/DataTitleComp.vue
@@ -1,10 +1,14 @@
 <template>
-$END$
+ <div class="text-center">
+   <h2 class="text-3xl font-bold">{{text}}}</h2>
+   <div class="text-2xl mt-4 mb-10">{{dataDate}}</div>
+ </div>
 </template>
 
 <script>
 export default {
-name: "DataTitleComp"
+  name: 'DataTitleComp',
+  props: ['text', 'dataDate']
 }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="home">
-Home
-  </div>
+  <main v-if="!loading">
+      Show Data
+  </main>
+
+  <main v-else class="flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" alt="" class="w-24 m-auto">
+  </main>
 </template>
 
 <script>
@@ -10,6 +17,32 @@ Home
 export default {
   name: 'HomeView',
   components: {
+  },
+  data () {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      status: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData () {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created () {
+    const data = await this.fetchCovidData()
+    console.log(data)
+
+    this.dataDate = data.Date
+    this.stats = data.Globle
+    this.countries = data.Countries
+    this.loading = false
   }
 }
 </script>
